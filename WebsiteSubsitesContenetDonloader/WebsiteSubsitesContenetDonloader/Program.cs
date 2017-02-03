c63e267e0e2ac764ebb6724c5e3076bd3ad3de0a
User <User@COMP>
2017-02-03 17:09:17 +0400

    Adding webclient

--- WebsiteSubsitesContenetDonloader/WebsiteSubsitesContenetDonloader/Program.cs
+++ WebsiteSubsitesContenetDonloader/WebsiteSubsitesContenetDonloader/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace WebsiteSubsitesContentDownloader
 {
@@ -26,11 +28,32 @@ namespace WebsiteSubsitesContentDownloader
         string all = string.Empty;
         string htmlCode = string.Empty;
 
+        using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+        {
+          htmlCode = client.DownloadString(path);
+          Console.WriteLine(htmlCode);
+          all = showMatch(htmlCode, @"<(http)\b[^>]*>");
+          Console.WriteLine("-------------");
+          Console.WriteLine("\nThere are the following URLs: ");
+          Console.WriteLine(all);
+          string[] split = all.Split(new Char[] { '"', '?' });
+          Console.WriteLine("-------------");
+          string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+          WebClient client1;
 
-
-
+        }
 
         Console.ReadKey();
+      }
+    }
+    private static string showMatch(string text, string expr)
+    {
+      MatchCollection mc = Regex.Matches(text, expr);
+      string result = "";
+      foreach (Match m in mc)
+      {
+        result += m.ToString() + "\n";
+      }
+      return result;
     }
   }
-}
