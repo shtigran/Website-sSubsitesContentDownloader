98e79b7319fec51155a2685026c68866bf99d708
User <User@COMP>
2017-02-03 17:07:11 +0400

    Program message

---
+++ WebsiteSubsitesContenetDonloader/WebsiteSubsitesContenetDonloader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteSubsitesContentDownloader
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Welcome message
+      Console.WriteLine("____Welcome Website Content Downloader____\n");
+      Console.Write("Please enter the website path: ");
+      string path = Console.ReadLine();
+
+
+      Console.ReadKey();
+    }
+  }
+}
